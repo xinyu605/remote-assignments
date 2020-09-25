62a280bbf7cecc11c16762b26c828d9111207f67
XinyuYan <xinyuyan605@gmail.com>
2020-09-25 11:28:38 +0800

    week3-assignment2 done

--- week-3/app.js
+++ week-3/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http');
 
 //create a server
 const app = express();
@@ -10,3 +11,28 @@ app.listen(3000);
 app.get('/', (req, res) => {
     res.send('Hello, My Server!');
 });
+
+app.get('/getData', (req, res) => {
+    const number = req.query.number;
+    const integer = /^\+?[1-9][0-9]*$/;
+    if (!number) {
+        res.send('Lack of parameter');
+    
+    // Assignment-2
+        
+    } else if (integer.test(number)) {
+        const ans = addNum(number);
+        res.send(`Result is ${ans}`);
+    } else {
+        res.send('Wrong parameter!')
+    }
+});
+
+//add positive numbers
+function addNum(integer) {
+    let sumNum = 0;
+    for (let i = 0; i <= integer; i++) {
+        sumNum += i;
+    }
+    return sumNum;
+}
