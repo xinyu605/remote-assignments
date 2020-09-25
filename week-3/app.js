8b80e8bd848e13c9bd415a958604d936c2428113
XinyuYan <xinyuyan605@gmail.com>
2020-09-25 19:26:24 +0800

    week3-assignment3 done

--- week-3/app.js
+++ week-3/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const http = require('http');
+const bodyParser = require('body-parser');
 
 //create a server
 const app = express();
+
+
+//Assignment-3: static middleware, body-parser
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 //set up the server using the listen method (tell server which port to serve)
 app.listen(3000);
@@ -24,11 +30,26 @@ app.get('/getData', (req, res) => {
         const ans = addNum(number);
         res.send(`Result is ${ans}`);
     } else {
-        res.send('Wrong parameter!')
+        res.send('Wrong parameter!');
     }
 });
 
-//add positive numbers
+//Assignment-3: user interaction
+app.post('/getData', (req, res) => {
+    const number = req.body.number;
+    const integer = /^\+?[1-9][0-9]*$/;
+    if (!number) {
+        res.send('Lack of parameter');        
+    } else if (integer.test(number)) {
+        const ans = addNum(number);
+        res.send(`Result is ${ans}`);
+    } else {
+        res.send('Wrong parameter!');
+    }
+});
+
+
+//Assignment-2: add positive numbers
 function addNum(integer) {
     let sumNum = 0;
     for (let i = 0; i <= integer; i++) {
