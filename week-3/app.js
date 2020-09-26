a5177aff4ce5e38db495bc61254ae2ed3821c1c1
XinyuYan <xinyuyan605@gmail.com>
2020-09-26 11:49:07 +0800

    week3-assignment4-form by POST method

--- week-3/app.js
+++ week-3/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 //create a server
 const app = express();
 
+//set pug as template engine
+app.set('view engine', 'pug');
 
-//Assignment-3: static middleware, body-parser
+//Assignment-3: static middleware, body-parser, cookie-parser
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 //set up the server using the listen method (tell server which port to serve)
 app.listen(3000);
@@ -48,6 +52,24 @@ app.post('/getData', (req, res) => {
     }
 });
 
+//Assignment-4
+app.get('/myName', (req, res) =>{
+    const name = req.cookies.name;
+    if(name){
+        res.render('myName', {name});   //{name: name}
+    }else{
+        res.redirect('/trackName');
+    }
+});
+
+app.get('/trackName', (req, res) => {
+    res.render('myName');
+});
+
+app.post('/trackName', (req, res) => {
+    res.cookie('name', req.body.name);  //save the cookie
+    res.redirect('/myName');
+});
 
 //Assignment-2: add positive numbers
 function addNum(integer) {
