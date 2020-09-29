bb0ee69cabf8d1c319943a8236f06ace2a552a02
XinyuYan <xinyuyan605@gmail.com>
2020-09-29 09:47:31 +0800

    week4-assignment1 done

---
+++ week-4/assignment-1.js
@@ -0,0 +1,11 @@
+//Assignment-1 callback function
+function delayedResult(n1, n2, delayTime, callback) {
+    const sum = n1 + n2;
+    window.setTimeout(() => {callback(sum)},delayTime);
+}
+delayedResult(4, 5, 3000, function (result) {
+    console.log(result);
+}); // 9 (4+5) will be shown in the console after 3 seconds 
+delayedResult(-5, 10, 2000, function (result) {
+    window.alert(result);
+}); // 5 (-5+10) will be shown in an alert dialog after 2 seconds
